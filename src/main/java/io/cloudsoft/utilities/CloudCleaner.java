39255d6ceb55c4e565b24c56f702e4d088b4bdb8
Andrea Turli <andrea.turli@gmail.com>
2013-08-08 12:25:56 +0200

    added rackspace US and ibm smartcloud

--- src/main/java/io/cloudsoft/utilities/CloudCleaner.java
+++ src/main/java/io/cloudsoft/utilities/CloudCleaner.java
@@ -1,18 +1,16 @@
 package io.cloudsoft.utilities;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
-import com.google.inject.Module;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Predicates.and;
 import io.cloudsoft.utilities.io.cloudsoft.utilities.model.Instance;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import org.jclouds.ContextBuilder;
 import org.jclouds.aws.ec2.AWSEC2Client;
 import org.jclouds.aws.ec2.services.AWSInstanceClient;
@@ -40,15 +38,20 @@ import org.jclouds.softlayer.domain.VirtualGuest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Predicates.and;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.base.Throwables;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.inject.Module;
+import com.ibm.cloud.api.rest.client.DeveloperCloud;
+import com.ibm.cloud.api.rest.client.DeveloperCloudClient;
 
 /**
  * Utility to detect long-running VMs, and to kill them
@@ -91,7 +94,7 @@ public class CloudCleaner {
 
    private static final List<String> SUPPORTED_PROVIDERS =
            ImmutableList.of(GOOGLE_COMPUTE_ENGINE_PROVIDER, SOFTLAYER_PROVIDER, AWS_PROVIDER,
-                   HPCLOUD_PROVIDER, RACKSPACE_UK_PROVIDER);
+                   HPCLOUD_PROVIDER, RACKSPACE_UK_PROVIDER, RACKSPACE_US_PROVIDER, IBM_SCE_PROVIDER);
    /**
     * System property for access key.
     */
@@ -142,128 +145,154 @@ public class CloudCleaner {
    private List<Instance> retrieveAndPrintInstances(List<String> providers) throws Exception {
       List<Instance> instances = Lists.newArrayList();
       for (String provider : providers) {
-         try {
-            computeServiceContext = getComputeServiceContext(provider);
-            if (provider.equals(AWS_PROVIDER)) {
-               List<Instance> ec2Instances = listEc2Instances(and(TagPredicates.isInstance()));
-               instances.addAll(ec2Instances);
-               printInstances(provider, ec2Instances);
-            } else if (provider.equals(RACKSPACE_UK_PROVIDER)) {
-               List<Instance> rackspaceUkInstances = listOpenstackInstances(RACKSPACE_UK_PROVIDER);
-               instances.addAll(rackspaceUkInstances);
-               printInstances(provider, rackspaceUkInstances);
-            } else if (provider.equals(HPCLOUD_PROVIDER)) {
-               List<Instance> hpCloudInstances = listOpenstackInstances(HPCLOUD_PROVIDER);
-               instances.addAll(hpCloudInstances);
-               printInstances(provider, hpCloudInstances);
-            } else if (provider.equals(SOFTLAYER_PROVIDER)) {
-               List<Instance> softlayerInstances = listSoftlayerInstances();
-               instances.addAll(softlayerInstances);
-               printInstances(provider, softlayerInstances);
-            } else if (provider.equals(GOOGLE_COMPUTE_ENGINE_PROVIDER)) {
-               List<Instance> googleComputeInstances = listGoogleComputeEngineInstances();
-               instances.addAll(googleComputeInstances);
-               printInstances(provider, googleComputeInstances);
-            } else {
-               throw new RuntimeException("Not supported api/provider: " + provider);
-            }
-         } finally {
-            computeServiceContext.close();
+         if (provider.equals(AWS_PROVIDER)) {
+            List<Instance> ec2Instances = listEc2Instances(and(TagPredicates.isInstance()));
+            instances.addAll(ec2Instances);
+            printInstances(provider, ec2Instances);
+         } else if (provider.equals(RACKSPACE_UK_PROVIDER) || provider.equals(RACKSPACE_US_PROVIDER)
+               || provider.equals(HPCLOUD_PROVIDER)) {
+            List<Instance> openstackInstances = listOpenstackInstances(provider);
+            instances.addAll(openstackInstances);
+            printInstances(provider, openstackInstances);
+         } else if (provider.equals(SOFTLAYER_PROVIDER)) {
+            List<Instance> softlayerInstances = listSoftlayerInstances();
+            instances.addAll(softlayerInstances);
+            printInstances(provider, softlayerInstances);
+         } else if (provider.equals(GOOGLE_COMPUTE_ENGINE_PROVIDER)) {
+            List<Instance> googleComputeInstances = listGoogleComputeEngineInstances();
+            instances.addAll(googleComputeInstances);
+            printInstances(provider, googleComputeInstances);
+         } else if (provider.equals(IBM_SCE_PROVIDER)) {
+            List<Instance> ibmSceInstances = listIbmSceInstances();
+            instances.addAll(ibmSceInstances);
+            printInstances(provider, ibmSceInstances);
+         } else {
+            throw new RuntimeException("Not supported api/provider: " + provider);
          }
+      }
+      return instances;
+   }
+
+   private List<Instance> listIbmSceInstances() {
+      List<Instance> instances = Lists.newArrayList();
+      DeveloperCloudClient client = DeveloperCloud.getClient();
+      client.setRemoteCredentials(credentials.get(IBM_SCE_PROVIDER).get(0), credentials.get(IBM_SCE_PROVIDER).get(1));
+      try {
+         for(com.ibm.cloud.api.rest.client.bean.Instance instance : client.describeInstances()) {
+            instances.add(Instance.builder().id(instance.getID()).provider(IBM_SCE_PROVIDER)
+                  .region(instance.getLocation()).type(instance.getInstanceType())
+                  .status(instance.getStatus().toString())
+                   .keyName(instance.getKeyName())
+                   .uptime(new Date().getTime() - instance.getLaunchTime().getTime())
+                  // .tags(nodeMetadata.getTags())
+                  .build());
+         }
+      } catch (Exception e) {
+         throw Throwables.propagate(e);
       }
       return instances;
    }
 
    private List<Instance> listGoogleComputeEngineInstances() {
       List<Instance> instances = Lists.newArrayList();
-      for(NodeMetadata nodeMetadata : computeServiceContext.getComputeService().listNodesDetailsMatching(Predicates
-              .<ComputeMetadata>notNull())) {
-         instances.add(Instance.builder()
-                 .id(nodeMetadata.getId())
-                 .provider(GOOGLE_COMPUTE_ENGINE_PROVIDER)
-                 .region(nodeMetadata.getLocation().getDescription())
-                 .type(nodeMetadata.getType().name())
-                 .status(nodeMetadata.getStatus().name())
-                         //.keyName(nodeMetadata.getType())
-                         //.uptime(new Date().getTime() - computeServiceContext.getComputeService().getTime())
-                         //.tags(nodeMetadata.getTags())
-                 .build());
+      try {
+         computeServiceContext = getComputeServiceContext(GOOGLE_COMPUTE_ENGINE_PROVIDER);
+         for (NodeMetadata nodeMetadata : computeServiceContext.getComputeService().listNodesDetailsMatching(
+               Predicates.<ComputeMetadata> notNull())) {
+            instances.add(Instance.builder().id(nodeMetadata.getId()).provider(GOOGLE_COMPUTE_ENGINE_PROVIDER)
+                  .region(nodeMetadata.getLocation().getDescription()).type(nodeMetadata.getType().name())
+                  .status(nodeMetadata.getStatus().name())
+                  // .keyName(nodeMetadata.getType())
+                  // .uptime(new Date().getTime() -
+                  // computeServiceContext.getComputeService().getTime())
+                  // .tags(nodeMetadata.getTags())
+                  .build());
+         }
+      } catch (Exception e) {
+         throw Throwables.propagate(e);
+      } finally {
+         computeServiceContext.close();
       }
       return instances;
    }
 
-   private List<Instance>  listSoftlayerInstances() {
+   private List<Instance> listSoftlayerInstances() {
       List<Instance> instances = Lists.newArrayList();
-      RestContext<SoftLayerClient, SoftLayerAsyncClient> client = computeServiceContext.unwrap();
-      SoftLayerClient api = client.getApi();
-      for(VirtualGuest virtualGuest :  api.getVirtualGuestClient().listVirtualGuests()) {
-         instances.add(Instance.builder()
-                 .id(virtualGuest.getUuid())
-                 .provider(SOFTLAYER_PROVIDER)
-                 .region(virtualGuest.getDatacenter().getLongName())
-                 .type("Cloud Compute Instance")
-                 .status(virtualGuest.getPowerState().toString())
-                 .keyName(virtualGuest.getAccountId() + "")
-                 .uptime(new Date().getTime() - virtualGuest.getCreateDate().getTime())
-                 //.tags()
-                 .build());
+      try {
+         computeServiceContext = getComputeServiceContext(SOFTLAYER_PROVIDER);
+         RestContext<SoftLayerClient, SoftLayerAsyncClient> client = computeServiceContext.unwrap();
+         SoftLayerClient api = client.getApi();
+         for (VirtualGuest virtualGuest : api.getVirtualGuestClient().listVirtualGuests()) {
+            instances.add(Instance.builder().id(virtualGuest.getUuid()).provider(SOFTLAYER_PROVIDER)
+                  .region(virtualGuest.getDatacenter().getLongName()).type("Cloud Compute Instance")
+                  .status(virtualGuest.getPowerState().toString()).keyName(virtualGuest.getAccountId() + "")
+                  .uptime(new Date().getTime() - virtualGuest.getCreateDate().getTime())
+                  // .tags()
+                  .build());
+         }
+      } catch (Exception e) {
+         throw Throwables.propagate(e);
+      } finally {
+         computeServiceContext.close();
       }
       return instances;
    }
 
    private List<Instance> listOpenstackInstances(String provider) {
       List<Instance> instances = Lists.newArrayList();
-      RestContext<NovaApi, NovaAsyncApi> client = computeServiceContext.unwrap();
-      for (String zone : client.getApi().getConfiguredZones()) {
-         ServerApi serverApiForZone = client.getApi().getServerApiForZone(zone);
-         FluentIterable<? extends Server> servers = serverApiForZone.listInDetail().concat();
+      try {
+         computeServiceContext = getComputeServiceContext(provider);
+         RestContext<NovaApi, NovaAsyncApi> client = computeServiceContext.unwrap();
+         for (String zone : client.getApi().getConfiguredZones()) {
+            ServerApi serverApiForZone = client.getApi().getServerApiForZone(zone);
+            FluentIterable<? extends Server> servers = serverApiForZone.listInDetail().concat();
 
-         for (Server server: servers) {
-            serverApiForZone.getMetadata(server.getId());
-            instances.add(Instance.builder()
-                    .id(server.getId())
-                    .provider(provider)
-                    .region(zone)
-                    .type(server.getFlavor().getId())
-                    .status(server.getStatus().toString())
-                    .keyName(server.getKeyName())
-                    .uptime(new Date().getTime() - server.getCreated().getTime())
-                    .tags(serverApiForZone.getMetadata(server.getId()))
-                    .build());
+            for (Server server : servers) {
+               serverApiForZone.getMetadata(server.getId());
+               instances.add(Instance.builder().id(server.getId()).provider(provider).region(zone)
+                     .type(server.getFlavor().getId()).status(server.getStatus().toString())
+                     .keyName(server.getKeyName()).uptime(new Date().getTime() - server.getCreated().getTime())
+                     .tags(serverApiForZone.getMetadata(server.getId())).build());
+            }
          }
+      } catch (Exception e) {
+         throw Throwables.propagate(e);
+      } finally {
+         computeServiceContext.close();
       }
       return instances;
    }
 
    /** TODO: filter all instances that are not `termination protection` flagged */
-   private List<Instance> listEc2Instances(Predicate predicate) throws Exception {
+   private List<Instance> listEc2Instances(Predicate predicate) {
       List<Instance> instances = Lists.newArrayList();
-      RestContext<EC2Client, EC2AsyncClient> client = computeServiceContext.unwrap();
-      for (String region : client.getApi().getConfiguredRegions()) {
-         TagApi tagApi = client.getApi().getTagApiForRegion(region).get();
-         Set<? extends Reservation<? extends RunningInstance>> reservations = getReservations(client, region);
-         for (Reservation reservation : reservations) {
-            for (Object aReservation : reservation) {
-               RunningInstance instance = (RunningInstance) aReservation;
-               ImmutableList<Tag> filteredTags = tagApi.list().filter(predicate).toList();
-               Map<String, String> tags = Maps.newHashMap();
-               for (Tag tag : filteredTags) {
-                  if (tag.getResourceId().equals(instance.getId())) {
-                     tags.put(tag.getKey(), tag.getValue().orNull());
+      try {
+         computeServiceContext = getComputeServiceContext(AWS_PROVIDER);
+         RestContext<EC2Client, EC2AsyncClient> client = computeServiceContext.unwrap();
+         for (String region : client.getApi().getConfiguredRegions()) {
+            TagApi tagApi = client.getApi().getTagApiForRegion(region).get();
+            Set<? extends Reservation<? extends RunningInstance>> reservations = getReservations(client, region);
+            for (Reservation reservation : reservations) {
+               for (Object aReservation : reservation) {
+                  RunningInstance instance = (RunningInstance) aReservation;
+                  ImmutableList<Tag> filteredTags = tagApi.list().filter(predicate).toList();
+                  Map<String, String> tags = Maps.newHashMap();
+                  for (Tag tag : filteredTags) {
+                     if (tag.getResourceId().equals(instance.getId())) {
+                        tags.put(tag.getKey(), tag.getValue().orNull());
+                     }
                   }
+                  instances.add(Instance.builder().id(instance.getId()).provider(AWS_PROVIDER).region(region)
+                        .type(instance.getInstanceType()).status(instance.getInstanceState().value())
+                        .keyName(instance.getKeyName())
+                        .uptime(new Date().getTime() - instance.getLaunchTime().getTime()).tags(tags).build());
                }
-               instances.add(Instance.builder()
-                       .id(instance.getId())
-                       .provider(AWS_PROVIDER)
-                       .region(region)
-                       .type(instance.getInstanceType())
-                       .status(instance.getInstanceState().value())
-                       .keyName(instance.getKeyName())
-                       .uptime(new Date().getTime() - instance.getLaunchTime().getTime())
-                       .tags(tags)
-                       .build());
             }
          }
+      } catch (Exception e) {
+         throw Throwables.propagate(e);
+      } finally {
+         computeServiceContext.close();
       }
       return instances;
    }
@@ -429,7 +458,7 @@ public class CloudCleaner {
       for(String provider : providers) {
          String identity = checkNotNull(System.getProperty(provider + "." + IDENTITY_PROPERTY), IDENTITY_PROPERTY + " for " + provider);
          String credential = checkNotNull(System.getProperty(provider + "." + CREDENTIAL_PROPERTY), CREDENTIAL_PROPERTY + " for " + provider);
-         LOG.info("provider({}) - identity({}), credential({})", provider, identity, credential);
+         LOG.info("Found credentials for provider({})", provider);
          credentials.put(provider, ImmutableList.of(identity, credential));
       }
    }
